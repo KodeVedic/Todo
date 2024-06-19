9192af97ae88e9bec3feeabb178b49e1b90a9e42
Vaibhav <vaibhav@kodekalp.com>
2024-06-19 11:06:20 +0530

    todo application code add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import './App.css';
-import Random from './Component/Random';
+import React, { useState, useEffect } from 'react';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
+import './style.css';
+
+const LOCAL_STORAGE_KEY = 'react-todo-list-todos';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  // const myvariable = [];
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storedTodos) setTodos(storedTodos);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
+  function addTodo(name) {
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: Math.random(), name: name, complete: false }];
+    });
+  }
+
+  function toggleTodo(id) {
+    setTodos(prevTodos => {
+      return prevTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, complete: !todo.complete };
+        }
+        return todo;
+      });
+    });
+  }
+
   return (
     <div className="App">
-      
-      <Random/>
+      <TodoForm addTodo={addTodo} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <div>
+        {todos.filter(todo => !todo.complete).length} left to do
+      </div>
     </div>
   );
 }
